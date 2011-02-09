c1b5e364062eadf4841ad015e0701b7ad630b9fa
Tarulia <takuja@gmx.de>
2011-02-09 16:25:27 +0100

    Fix Typo in f09e289349ce3fc1a1a6184835436ba256b26135

--- sql/updates/world/2011_02_08_lol_3_world_creature_template.sql
+++ sql/updates/world/2011_02_08_lol_3_world_creature_template.sql
@@ -1,2 +1,2 @@
 -- 14845 Stamp Thunderhorn - add Vendor Menu
-UPDATE `creature_template` SET `npc_flags` = `npc_flags`|1|128 WHERE `entry` = 14845;
+UPDATE `creature_template` SET `npcflag` = `npcflag`|1|128 WHERE `entry` = 14845;
