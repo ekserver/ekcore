6098b7614f2bfa5a6b135fe2ea6026b134037163
Tarulia <takuja@gmx.de>
2011-02-14 03:10:29 +0100

    DB/Items: Set Class to fixed 64, cant use binary addition

--- sql/updates/world/2011_02_08_lol_0_world_item_template.sql
+++ sql/updates/world/2011_02_08_lol_0_world_item_template.sql
@@ -1,2 +1,2 @@
 -- Correct Savage Gladiator's Totems
-UPDATE `item_template` SET `flags` = `flags`|4096 , `AllowableClass` = `AllowableClass`|64 , `maxcount` = 1, `TotemCategory` = 21 WHERE `entry` IN (42595, 42594, 42593);
+UPDATE `item_template` SET `flags` = `flags`|4096 , `AllowableClass` = 64 , `maxcount` = 1, `TotemCategory` = 21 WHERE `entry` IN (42595, 42594, 42593);
