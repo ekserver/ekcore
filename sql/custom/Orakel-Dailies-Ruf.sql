62fb96aab52edbc4f32d134b823c9c29bfc15c15
Hirndille <hirndille@gmx.de>
2011-02-17 13:16:16 +0100

    DB/Quests Fix Reputation for Oracle Daily-Quests

---
+++ sql/custom/Orakel-Dailies-Ruf.sql
@@ -0,0 +1,8 @@
+UPDATE `world`.`quest_template` SET `RewRepValue1`='250' WHERE `entry`='12704';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='350' WHERE `entry`='12705';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='250' WHERE `entry`='12726';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='250' WHERE `entry`='12735';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='250' WHERE `entry`='12736';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='250' WHERE `entry`='12737';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='350' WHERE `entry`='12761';
+UPDATE `world`.`quest_template` SET `RewRepValue1`='350' WHERE `entry`='12762';
