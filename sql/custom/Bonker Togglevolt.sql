1cd2b0a0f98314b9d9de27c89477d0a31483cd50
Hirndille <hirndille@gmx.de>
2011-02-22 18:26:35 +0100

    DB/Spawns Spawn Bonker Togglevolt for Quest 11673

---
+++ sql/custom/Bonker Togglevolt.sql
@@ -0,0 +1,5 @@
+INSERT INTO `creature` (`id`, `map`, `spawnMask`, `phaseMask`, `modelid`,`equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`,
+`spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`,`DeathState`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`) VALUES
+
+( '25589', '571', '1', '1', '0', '0', '4438.3', '5414.56', '-14.7768', '5.01766', '300', '0', '0', '13937', '0', '0', '0', '0', '0', '0'); 
+
