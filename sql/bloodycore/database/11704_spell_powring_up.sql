2dfd8e9acec2d09c64e9b3651e83bb9df4b664c9
dimiandre <dimiandre@gmail.com>
2011-03-04 15:23:14 +0100

    [Toc] Add missing SQL for spell_powering_up scriptname

---
+++ sql/bloodycore/database/11704_spell_powring_up.sql
@@ -0,0 +1,6 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (67590,67602,67603,67604) AND `ScriptName`='spell_powering_up';
+INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(67590, 'spell_powering_up'),
+(67602, 'spell_powering_up'),
+(67603, 'spell_powering_up'),
+(67604, 'spell_powering_up');
