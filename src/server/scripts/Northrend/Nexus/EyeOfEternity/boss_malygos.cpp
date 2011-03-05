ekserver/ekcore

commit e6bcb8fba1c2ae93cf362fe6607a6ddd01ee71e4

Scripts/Malygos: try to tempfix own disc movement for nexus lords

src/server/scripts/Northrend/Nexus/EyeOfEternity/boss_malygos.cpp
@@ -972,10 +972,12 @@ public:
         npc_hover_discAI(Creature* pCreature) : ScriptedAI(pCreature) {}
 
         uint32 count;
+        uint32 uiCheckTimer;
 
         void Reset()
         {
             count = 1;
+            uiCheckTimer = 1*IN_MILLISECONDS;
         }
 
         void PassengerBoarded(Unit* pWho, int8 /*seatId*/, bool apply)
@@ -1024,7 +1026,21 @@ public:
             }
         }
 
-        void UpdateAI(const uint32 uiDiff) {}
+        void UpdateAI(const uint32 uiDiff)
+        {
+            if (me->GetReactState() == REACT_AGGRESSIVE)
+            {
+                if (uiCheckTimer <= uiDiff)
+                {
+                    Unit* pUnit = me->GetVehicleKit()->GetPassenger(0);
+                    if (pUnit && pUnit->GetTypeId() == TYPEID_UNIT && pUnit->GetEntry() == NPC_NEXUS_LORD)
+                    {
+                        me->AI()->AttackStart(pUnit->getVictim());
+                    }
+                    uiCheckTimer = 1*IN_MILLISECONDS;
+                } else uiCheckTimer -= uiDiff;
+            }
+        }
     };
 
     CreatureAI* GetAI(Creature* pCreature) const

commit bd6c6edd174d3e89e2dcf69879f895911bff75fa

Scripts/Malygos: try to fix crash introduced in e6bcb8fba1c2ae93cf362fe6607a6ddd01ee71e4

src/server/scripts/Northrend/Nexus/EyeOfEternity/boss_malygos.cpp
@@ -983,7 +983,7 @@ public:
         void PassengerBoarded(Unit* pWho, int8 /*seatId*/, bool apply)
         {
             if (!apply)
-                me->ForcedDespawn();
+                me->ForcedDespawn(1*IN_MILLISECONDS); // TODO: remove the disc to lord aggro "copy"
         }
 
         void SetData(uint32 /*type*/, uint32 data)
@@ -1028,6 +1028,9 @@ public:
 
         void UpdateAI(const uint32 uiDiff)
         {
+            if (!UpdateVictim())
+                return;
+
             if (me->GetReactState() == REACT_AGGRESSIVE)
             {
                 if (uiCheckTimer <= uiDiff)
@@ -1035,7 +1038,12 @@ public:
                     Unit* pUnit = me->GetVehicleKit()->GetPassenger(0);
                     if (pUnit && pUnit->GetTypeId() == TYPEID_UNIT && pUnit->GetEntry() == NPC_NEXUS_LORD)
                     {
-                        me->AI()->AttackStart(pUnit->getVictim());
+                        Unit* pTarget = pUnit->getVictim();
+                        if (pTarget && pTarget != me->getVictim())
+                        {
+                            me->getThreatManager().modifyThreatPercent(me->getVictim(), -100);
+                            me->AddThreat(pTarget, 9999999.0f);
+                        }
                     }
                     uiCheckTimer = 1*IN_MILLISECONDS;
                 } else uiCheckTimer -= uiDiff;