2acdfe0591e040b35efa1da2c6afcd2e1d86ffc8
zwerg <seaofgreen@gmx.net>
2011-03-10 16:19:24 +0100

    Scripts/AzjolNerub: try to balance anubarak encounter

---
+++ sql/updates/world/2011_03_10_lol_0_world_creature_ai_scripts.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_ai_scripts` SET `event_param1`=5000, `event_param2`=10000, `event_param3`=4000, `event_param4`=9000 WHERE `id` IN (2921701, 2921702);
+UPDATE `creature_ai_scripts` SET `event_param1`=10000, `event_param2`=35000, `event_param3`=15000, `event_param4`=20000 WHERE `id` IN (2921703, 2921704);
---
+++ sql/updates/world/2011_03_10_lol_1_world_creature_template.sql
@@ -0,0 +1,4 @@
+UPDATE `creature_template` SET `dmg_multiplier`=0.2 WHERE `entry` IN (29209,31647);
+UPDATE `creature_template` SET `dmg_multiplier`=0.5 WHERE `entry` IN (29213,31597);
+UPDATE `creature_template` SET `dmg_multiplier`=0.75 WHERE `entry` = 29217;
+UPDATE `creature_template` SET `dmg_multiplier`=1.25 WHERE `entry` = 31607;
