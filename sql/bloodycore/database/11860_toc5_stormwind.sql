a4a23ab060b97acbb65c0eb5a42b4e5be8f4b22e
dimiandre <dimiandre@gmail.com>
2011-03-16 17:49:46 +0100

    [Toc5] Template of Stormwind champions, fix summon memory
    Fix react of trash of paletress

---
+++ sql/bloodycore/database/11860_toc5_stormwind.sql
@@ -0,0 +1,4 @@
+-- Stormwind steed template 
+UPDATE `creature_template` SET `rank`=1, `Health_mod`=5, `faction_A`=16, `faction_H`=16, `ScriptName`='generic_vehicleAI_toc5' WHERE `entry`=33217;
+-- Stomwind champion template
+UPDATE `creature_template` SET `unit_flags`=33554434, `AIName`='' WHERE `entry`=35328;
