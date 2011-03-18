dcc2ac0fca99217063033866367bd548df09c90a
Hirndille <hirndille@gmx.de>
2011-03-18 09:36:20 +0100

    DB/Quests Fix Quest 13069 Shoot 'em up, thanks to treak and pitcrawler

---
+++ sql/updates/world/2011_03_18_lol_5_world_quest_template.sql
@@ -0,0 +1,9 @@
+-- Quest Fix 13069 Shoot 'em up! 
+
+-- Disable Move of Vehicle (30337)
+DELETE FROM `creature_template` WHERE entry = 30337;
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction_A`, `faction_H`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `Health_mod`, `Mana_mod`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) VALUES 
+(30337, 0, 0, 0, 0, 0, 26757, 0, 0, 0, 'Jotunheim Rapid-Fire Harpoon', '', 'vehichleCursor', 0, 80, 80, 2, 35, 35, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2000, 0, 1, 4, 8, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 56570, 0, 0, 0, 0, 0, 0, 229, 0, 0, '', 0, 3, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, '', 12340);
+UPDATE `world` . `creature` SET unit_flags = 4 WHERE id = 30337;
+-- Kill Credit Fix
+UPDATE `quest_template` SET ReqSpellCast1 = 56578 WHERE entry = 13069;
