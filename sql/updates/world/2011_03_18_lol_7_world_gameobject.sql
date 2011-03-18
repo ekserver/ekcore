9742f543bd26fe0537a950e0b679263ac7fdd29b
Hirndille <hirndille@gmx.de>
2011-03-18 10:00:54 +0100

    DB/Spawns Fix Spawn for Quest Bijou's Belongings, thanks to treak

---
+++ sql/updates/world/2011_03_18_lol_7_world_gameobject.sql
@@ -0,0 +1,2 @@
+DELETE FROM `gameobject` WHERE `id` = 175334;
+INSERT INTO `gameobject` (`id`, `map`, `spawnMask`, `phaseMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES (175334, 229, 1, 1, 35.6104, -445.744, -18.7806, 5.89444, 0, 0, 0.193153, -0.981169, 300, 0, 1);
