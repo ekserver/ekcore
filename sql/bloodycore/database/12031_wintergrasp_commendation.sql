f245cb874ede3b3fb9425d8fca4723b536780bcf
SimonDMII <simonlathuile@hotmail.it>
2011-03-28 18:49:18 +0200

    [DB] Add Wintergrasp Commendation to vendor for 9 Wintergrasp Mark of Honor.

---
+++ sql/bloodycore/database/12031_wintergrasp_commendation.sql
@@ -0,0 +1,5 @@
+DELETE FROM `npc_vendor` WHERE `item` = 44115 AND `ExtendedCost` = 2576;
+INSERT INTO `npc_vendor` (`entry`, `slot`, `item`, `maxcount`, `incrtime`, `ExtendedCost`) VALUES
+(32294, 0, 44115, 0, 0, 2576),
+(32296, 0, 44115, 0, 0, 2576);
+
