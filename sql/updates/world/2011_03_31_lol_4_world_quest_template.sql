54d2de2e7ab95b6bf3641092284cff5a208d9c4c
Hirndille <hirndille@gmx.de>
2011-03-31 22:37:11 +0200

    DB/Quests Fix Reputation for Bloodsail/Bootybay Quests

---
+++ sql/updates/world/2011_03_31_lol_4_world_quest_template.sql
@@ -0,0 +1,14 @@
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-5, `RewRepValueId2`=5 WHERE `entry`=662;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-2, `RewRepValueId2`=2 WHERE `entry`=663;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-5, `RewRepValueId2`=5 WHERE `entry`=664;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-5, `RewRepValueId2`=5 WHERE `entry`=665;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-6, `RewRepValueId2`=6 WHERE `entry`=666;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-6, `RewRepValueId2`=6 WHERE `entry`=667;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-3, `RewRepValueId2`=3 WHERE `entry`=668;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-5, `RewRepValueId2`=5 WHERE `entry`=669;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=-5, `RewRepValueId2`=5 WHERE `entry`=670;
+
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=577 `RewRepValueId1`=-7, `RewRepValueId2`=7 WHERE `entry`=9266;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=470 `RewRepValueId1`=-7, `RewRepValueId2`=7 WHERE `entry`=9267;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=369 `RewRepValueId1`=-7, `RewRepValueId2`=7 WHERE `entry`=9268;
+UPDATE `quest_template` SET `RewRepFaction1`=87, `RewRepFaction2`=21 `RewRepValueId1`=2, `RewRepValueId2`=-5 WHERE `entry`=9272;
