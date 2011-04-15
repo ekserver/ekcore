381c2cb027fd60cfdd539448ff6d5205b0f35e14
Valuur <valuur@googlemail.com>
2011-04-15 18:07:19 +0200

    - Add accountid to character_rates table

--- sql/custom/character_rates.sql
+++ sql/custom/character_rates.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS `character_rates`;
 CREATE TABLE `character_rates` (
         `guid`  INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Global Unique Identifier' ,
+        `account` INT(10) UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Account Id' ,
         `start_time`  TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Absolute start date, the bonus will be granted' ,
         `end_time`  TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Absolute end date, the bonus is expired' ,
         `kill_xp_rate`  INT(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Experience Multiplier' ,
         `quest_xp_rate`  INT(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Experience Multiplier' ,
         `explore_xp_rate`  INT(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Experience Multiplier' ,
         `rest_xp_rate` INT(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Experience Multiplier' ,
-        `money_drop_rate` INT(10) UNSIGNED NOT NULL DEFAULT 1 COMMENT 'Money Drop Rate' ,
 PRIMARY KEY (`guid`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
  
 -- Copy all players in characters into character_xp_rate with default values
-INSERT IGNORE INTO `character_rates` (`guid`) SELECT `guid` FROM `characters`;
+INSERT IGNORE INTO `character_rates` (`guid`, `account`) SELECT `guid`, `account` FROM `characters`;
